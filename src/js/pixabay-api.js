3d6e425ebda28a4ce32b5cffaa903ab44ce223a7
Nataliia Hodnia <156535926+nataliiahodnia@users.noreply.github.com>
2024-04-13 12:48:21 +0200

    try to do

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,4 +1,6 @@
-export function getPhotos(query) {
+import axios from 'axios';
+
+export async function getPhotos(query,page) {
   const API_KEY = '43339285-ea15b1a3e45dc18314900d076';
   const baseUrl = 'https://pixabay.com/api/';
   // const endPoint = "/search/photos";
@@ -9,13 +11,23 @@ export function getPhotos(query) {
     orientation: 'horizontal',
     image_type: 'photo',
     safesearch: true,
+    page,
+    per_page: 15,
   });
 
-  // return fetch(`${baseUrl}${endPoint}?${params}`).then((res) => {
-  return fetch(`${baseUrl}?${params}`).then(res => {
-    if (!res.ok) {
-      throw new Error(res.status);
-    }
-    return res.json();
-  });
+  // return fetch(`${baseUrl}?${params}`).then(res => {
+  //   if (!res.ok) {
+  //     throw new Error(res.status);
+  //   }
+  //   return res.json();
+  // });
+
+  try {
+    const response = await axios.get(`${baseUrl}?${params}`);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response.status);
+  }
+  
 }
+
--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
 // Додатковий імпорт стилів
 import 'izitoast/dist/css/iziToast.min.css';
+import axios from 'axios';
 
 import { getPhotos } from './js/pixabay-api.js';
 import { creategalleryMarkup } from './js/render-functions.js';
@@ -15,6 +16,13 @@ const list = document.querySelector('.gallery');
 const form = document.querySelector('.js-form');
 const loader = document.querySelector('.loader');
 const gallery = new SimpleLightbox('.gallery a');
+
+let currentPage = 1;
+let searchQuery = '';
+
+loadMoreBtn.style.display = 'none';
+loader.style.display = 'none';
+
 
 export function showLoader() {
   loader.classList.remove('is-hidden');
